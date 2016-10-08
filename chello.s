6e2bfa44ad1128cf3d1f95728c04a6a194f8d629
Ching Wong <ching.wong15@imperial.ac.uk>
2016-10-08 18:35:53 +0100

    On Question 4

---
+++ chello.s
@@ -0,0 +1,34 @@
+	.file	"chello.c"
+	.globl	message
+	.data
+	.align 8
+	.type	message, @object
+	.size	message, 14
+message:
+	.string	"Hello World!\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$13, %edx
+	movl	$message, %esi
+	movl	$1, %edi
+	call	writeA
+	movl	$0, %edi
+	call	exitA
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.2) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits
