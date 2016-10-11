0bfcb57bded478c4c7b308fb5bf6fde31dabeab9
Ching Wong <ching.wong15@imperial.ac.uk>
2016-10-11 09:06:23 +0100

    Added error messages for Question7

--- last_touch.sh
+++ last_touch.sh
@@ -2,11 +2,14 @@
 
 #!/bin/bash
 
+
+
+dir= `$1`
+echo "$dir"
 # to print out the last commit, use -n 1
 # The %aI shows the UTC time-stamp
 # The %h shows therevision_id
 # The %an shows the author
-
 git=`git ls-files | while read file;\
 do git log -n 1\
   --pretty="%aI  [%h]:$file     (%an)" -- $file; \
